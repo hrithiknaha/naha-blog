5ac3164bb58814ff0ecad34043249d7ac564e230
Hrithik Naha <naha.hrithkk@gmail.com>
2020-07-16 22:20:29 +0530

    Refactored routes

--- index.js
+++ index.js
@@ -6,32 +6,11 @@ const app = express();
 //DB Connection
 require('./config/db');
 
-//Models
-const Blogs = require('./models/blog');
-
 //Body
 app.use(express.json());
 
 //Routes
-app.get('/hello', (req, res) => {
-	res.send('hello World!').status(200);
-});
-
-app.get('/blogs', (req, res) => {
-	Blogs.find((err, blogs) => {
-		if (err) return console.log('Something went wrong ' + err);
-		if (blogs.length === 0) return res.json('No data found');
-		return res.json(blogs);
-	});
-});
-
-app.post('/blogs/new', (req, res) => {
-	const blogs = new Blogs(req.body);
-	blogs.save((err, savedBlog) => {
-		if (err) return console.log('Unable to save ' + err);
-		return res.json(savedBlog);
-	});
-});
+app.use('/', require('./routes'));
 
 const PORT = 3000;
 app.listen(PORT || process.env.PORT, () =>
---
+++ routes/index.js
@@ -0,0 +1,22 @@
+const router = require('express').Router();
+
+//Models
+const Blogs = require('../models/blog');
+
+router.get('/blogs', (req, res) => {
+	Blogs.find((err, blogs) => {
+		if (err) return console.log('Something went wrong ' + err);
+		if (blogs.length === 0) return res.json('No data found');
+		return res.json(blogs);
+	});
+});
+
+router.post('/blogs/new', (req, res) => {
+	const blogs = new Blogs(req.body);
+	blogs.save((err, savedBlog) => {
+		if (err) return console.log('Unable to save ' + err);
+		return res.json(savedBlog);
+	});
+});
+
+module.exports = router;
